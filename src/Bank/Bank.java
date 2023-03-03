6dba7ca37e97fbd59432f3acbdd24e0e2e0350fc
shivanikadam1 <shivanikadam707@gmail.com>
2023-03-03 10:24:00 +0530

    commit message

---
+++ src/Bank/Bank.java
@@ -0,0 +1,151 @@
+package Bank;
+
+import java.util.Scanner;
+
+public class Bank {
+	double balance;
+	int previousTransaction;
+	String customerName;
+	int accno;
+	String accType;
+	
+	
+
+	public Bank()
+	{
+		
+	}
+	
+	
+	
+	public Bank(double balance, int previousTransaction, String customerName, int accno, String customerId,
+			String accType) {
+		super();
+		this.balance = balance;
+		this.previousTransaction = previousTransaction;
+		this.customerName = customerName;
+		this.accno = accno;
+		
+		this.accType = accType;
+		
+	}
+	
+
+
+	void createAccount()
+	{
+		Scanner sc=new Scanner(System.in);
+	
+	  System.out.println("Enter the Name :");
+	  customerName=sc.next();
+	   System.out.println("Enter the Account Type : ");
+	   accType=sc.next();
+	   System.out.println("Enter the Account Number :");
+	   accno=sc.nextInt();
+	   System.out.println("Enter the Initial Balence :");
+	   balance=sc.nextDouble();
+	  System.out.println("Successfully Create Account....");
+	}
+	 void checkAccount() {
+		
+		 System.out.println("-----------------------------------------------------------------------------");
+		System.out.printf("|%-10s|%-14s|%-10s|%-8s|%n","ACCOUNTNUMBER","CUSTOMERNAME","ACCOUNTTYPE","BALENCE");
+		System.out.println("-------------------------------------------------------------------------------");
+		System.out.printf("|%-14s|%-14s|%-10s|%-8s|%n",accno,customerName,accType,balance);
+		System.out.println("-------------------------------------------------------------------------------");
+	 }
+	void deposit() {
+		int amount=0;
+		Scanner scanner=new Scanner(System.in);
+	    System.out.println("Enter Your Account Number: ");
+		int acc=scanner.nextInt();
+		if(accno==acc)
+		{
+		System.out.println("-------------------------------------------------------");
+		System.out.println("Enter an amount to deposit ");
+		System.out.println("-------------------------------------------------------");
+		
+		 amount = scanner.nextInt();
+		
+		  if(amount != 0) {
+				balance = balance + amount;
+				previousTransaction = amount;
+		
+		System.out.println("Successfully Deposit Your Amount...");
+		}
+		}
+		else
+		{
+			System.out.println("Sorry!!..Your Account Is Not Found");
+		}
+		System.out.println(" ");
+		
+		
+		}
+	
+	
+	void withdraw() {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter Your Account Number: ");
+		int acc1=scanner.nextInt();
+		if(accno==acc1)
+		{
+			
+		System.out.println("-------------------------------------------------------");
+		System.out.println("Enter an amount to withdraw ");
+		System.out.println("-------------------------------------------------------");
+		
+		int amount2 = scanner.nextInt();
+		
+		 if(balance>amount2  )
+		{
+			
+		  if(amount2 != 0) {
+					balance = balance - amount2;
+					previousTransaction = -amount2;
+					
+				}
+		System.out.println("Your Amount Successfully Withdraw");
+		System.out.println("\n");
+		}
+		 else {
+		 System.out.println("Your Balence is low...");
+		 }
+		 
+		}
+		else
+		{
+		System.out.println("Sorry!!..Your Account Is Not Found");
+		}
+		
+		
+	}
+	void checkbalence()
+	{
+		System.out.println("-------------------------------------------------------");
+		System.out.println(" Current Balance Is = "+balance);
+		System.out.println("-------------------------------------------------------");
+		System.out.println("");
+	}
+	
+	void getPreviousTransaction() {
+		if(previousTransaction > 0) {
+			System.out.println("Deposited: " + previousTransaction);
+		}
+		else if(previousTransaction < 0) {
+			System.out.println("Withdraw: " +Math.abs(previousTransaction)/*previousTransaction*/);
+		}
+		
+		else {
+			System.out.println("No Transaction Occured");
+		}
+		
+	}
+	void exit() {
+		System.out.println("Thank You for Using our Service.....!!");
+		System.exit(0);
+	}
+	
+
+
+}
---
+++ src/Bank/BankMain.java
@@ -0,0 +1,79 @@
+package Bank;
+
+import java.util.Scanner;
+
+public class BankMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bank b=new Bank();
+		
+		Scanner sc = new Scanner(System.in);
+		int option;
+		System.out.println("=================================================");
+		System.out.println("Online Banking System");
+		System.out.println("=================================================");
+	
+		System.out.println("1 : Create New Account");
+		System.out.println("2 : Display Account Details");
+		System.out.println("3 : Deposit");
+		System.out.println("4 : Withdraw");
+		System.out.println("5 : Check Balence");
+		System.out.println("6 : Check Your Previous Transaction");
+		System.out.println("7 : Exit The System");
+		
+		do {
+			System.out.println("=================================================================");
+			System.out.println("Enter Your Choice");
+			
+			option = sc.nextInt();			
+			
+			
+			switch (option) {
+			
+			case 1:
+				System.out.println("==============================================================");
+				b.createAccount();
+				System.out.println("\n");
+				break;
+			case 2:
+				System.out.println("===============================================================");
+				b.checkAccount();
+				System.out.println("\n");
+				break;
+				
+			case 3:
+				System.out.println("===============================================================");
+				b.deposit();
+				System.out.println("\n");
+				break;
+				
+			case  4:
+	
+				b.withdraw();
+				System.out.println("\n");
+				break;
+			case 5:
+				System.out.println("=============================================================");
+				b.checkbalence();
+				break;
+			case  6:
+				System.out.println("-------------------------------------------------------");
+				b.getPreviousTransaction();
+				System.out.println("-------------------------------------------------------");
+				System.out.println("\n");
+				break;
+				
+			case 7:
+				b.exit();
+				break;
+
+			
+			}			
+		}
+		while(option != 8);
+		
+
+	}
+
+}
